ef3091935ef02ae66985d5dffa4e393da77351cd
Roman Hartig <roman.hartig@seznam.cz>
2024-03-13 14:20:55 +0100

    Add product and reviews boilerplate resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
